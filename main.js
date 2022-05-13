7578a7e589b9465a758c5d0ee04736d034d196f9
Siyadav15 <98370835+Siyadav15@users.noreply.github.com>
2022-05-13 17:46:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function gotResults(error, results){
 
     }
     else{
-        console.log('results');
+        console.log(results);
         random_number_r=Math.floor(Math.random()*255)+1;
         random_number_g=Math.floor(Math.random()*255)+1;
         random_number_b=Math.floor(Math.random()*255)+1;
